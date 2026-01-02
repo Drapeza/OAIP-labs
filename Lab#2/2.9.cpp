562d980d6b7c70d43332e092ce3587bb9ce928ee
wexllerr <arinadrapeza@gmail.com>
2026-01-03 01:57:52 +0300

    Update 2.9.cpp

--- Lab#2/2.9.cpp
+++ Lab#2/2.9.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
 
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int choice,a,b;
+	cout << "Введите числа" << '\n';
+	cin >> a >> b;
+	cout << "Выберете действие" << '\n'
+		<< "1. a+b" << '\n'
+		<< "2. abs(a-b)" << '\n'
+		<< "3. a*b" << '\n'
+		<< "4. a/b" << '\n'
+		<< "5. b/a" << '\n';
+	cin >> choice;
+	switch (choice) {
+	case 1: { cout << a + b; break; }
+	case 2: { cout << abs(a - b); break; }
+	case 3: { cout << a * b; break; }
+	case 4: {
+		float n;
+		n =(float) a / b;
+		cout << n; break;
+	}
+	case 5: {
+	float n;
+	n =(float) b / a;
+	cout << n; break;
+	}
+	}
+	return 0;
+}
